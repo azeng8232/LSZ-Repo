372bddb06c546f0f55c21b9baecd48a4df3e294a
compscifan6336 <alanrayshaw@gmail.com>
2015-04-09 09:58:09 -0500

    Added Encoder

---
+++ src/Encoder.java
@@ -0,0 +1,43 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Encoder {
+
+	private File text;
+	
+	
+	public Encoder(String filename){
+		text = new File("encoded.txt");
+	}
+	public void write(String plaintext){
+		int[] encodedData = new int[plaintext.length()];
+		for(int i = 0; i < plaintext.length(); i++)
+		{
+			char[] chars = plaintext.toCharArray();
+			encodedData[i] = (int)chars[i];
+		}
+		for(int i = 0; i < plaintext.length(); i++){
+			System.out.print(encodedData[i] + " ");
+		}
+	}
+	
+	public List<String> read(){
+		List<String> normalData = new ArrayList<String>();
+		for(int i = 0; i < test.length; i++){
+			normalData.set(i, "" + (char)test[i]);
+		}
+		for(int i = 0; i < normalData.size(); i++){
+			System.out.println(normalData.get(i));
+		}
+		return normalData;
+		
+	}
+	public static void main(String[] args)
+	{
+		Encoder en = new Encoder("test");
+		en.write("chubs is a poopy");
+		en.read();
+	}
+}
